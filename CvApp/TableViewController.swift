11123ebb1d186994564dc586f26bababa089e2db
Adam <adam99.sturesson@hotmail.se>
2019-11-04 15:57:11 +0100

    Made a new view with the table view but it hasn't been filled yet

---
+++ CvApp/TableViewController.swift
@@ -0,0 +1,41 @@
+//
+//  TableViewController.swift
+//  CvApp
+//
+//  Created by Adam Sturesson on 2019-11-04.
+//  Copyright © 2019 Adam Sturesson. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+    
+    var experiences:[LifeExperiences] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 2
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCustomCell", for: indexPath)
+        return cell
+    }
+
+}
---
+++ CvApp/experience.swift
@@ -0,0 +1,17 @@
+//
+//  experience.swift
+//  CvApp
+//
+//  Created by Adam Sturesson on 2019-11-04.
+//  Copyright © 2019 Adam Sturesson. All rights reserved.
+//
+
+import UIKit
+
+class experience{
+    let image: UIImageView
+    let describingLabel: String
+    let dateLabel : String
+    
+    
+}
