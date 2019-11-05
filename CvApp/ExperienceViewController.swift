3a26e391a3325fa03a24e3c98bdeb9a76da14353
Adam Sturesson <adam99.sturesson@hotmail.se>
2019-11-05 15:50:35 +0100

    I made the table so it shows the different sections with and which elements it contains

--- CvApp/ExperienceViewController.swift
+++ CvApp/ExperienceViewController.swift
@@ -13,8 +13,11 @@ class ExperienceViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     
     var experiences: [Experience] = []
-    var arr: [(String,Experience)] = []
     
+    var data:[[Experience]] = [[]]
+    
+    let headerTitles = ["Jobs","Education"]
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self as? UITableViewDelegate
@@ -30,31 +33,38 @@ class ExperienceViewController: UIViewController {
         experiences.append(experience3)
         experiences.append(experience4)
         
-        arr.append(("Jobs",experiences[0]))
-        arr.append(("Jobs",experiences[1]))
-        arr.append(("Education",experiences[2]))
-        arr.append(("Education",experiences[3]))
-        
+        data = [[experiences[0],experiences[1]],[experiences[2],experiences[3]]]
         tableView.reloadData()
         
         }
-       
     }
+
 extension ExperienceViewController:UITableViewDelegate, UITableViewDataSource{
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return experiences.count
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section < headerTitles.count {
+            return headerTitles[section]
+        }
+        return nil
     }
-    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return data.count
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        data[section].count
+       }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if let cell = tableView.dequeueReusableCell(withIdentifier: "MyCustomCell", for: indexPath) as? LifeExperiencesTableViewCell{
-            let experience = experiences[indexPath.row]
-            cell.ImageInCell.image = UIImage(named:experience.image)
+            let experience: Experience=data[indexPath.section][indexPath.row]
+            cell.ImageInCell.image = UIImage(named: experience.image)
             cell.WorkeNameInCell.text = experience.describingLabel
             cell.datesInCell.text = experience.dateLabel
-            
+
             return cell
         }
         return UITableViewCell()
+    }
+    func tableView(_tableView: UITableView,didSelectRowAt indexPath:IndexPath){
+     
     }
  
   
---
+++ CvApp/SkillsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SkillsViewController.swift
+//  CvApp
+//
+//  Created by Adam Sturesson on 2019-11-05.
+//  Copyright © 2019 Adam Sturesson. All rights reserved.
+//
+
+import UIKit
+
+class SkillsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
